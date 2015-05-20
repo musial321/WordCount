071988352d10b605da0410bdaec1137c725628da
Christian Musial <musial321@gmail.com>
2015-05-20 10:22:43 -0400

    Corrected formatting

--- WordCount/src/WordCount.java
+++ WordCount/src/WordCount.java
@@ -1,110 +1,88 @@
 import java.io.*;
 
-
-public class WordCount 
-{
-	public static void main(String [] args) throws IOException
-	{
+public class WordCount {
+	public static void main(String [] args) throws IOException {
+		
 		BufferedReader reader;
 		
-		
-		
-		if(!parseArgs(args))
-		{
+		if(!parseArgs(args)) {
+			
 			System.out.println("Argument error");
 		}
-		else
-		{
+		else {
+			
 			File input = new File(args[args.length-1]);
 			reader = new BufferedReader(new FileReader(input));
-			
-			
-				try
-				{
+		
+				try {
 					int current;
 					int previous=0;
 					int wordCount=0;
 					
+					current=reader.read();
 					
-					
-						current=reader.read();
-						if(current!=32 && current!=13 && current!=-1 && current!=10)
-						{
+						if(current!=32 && current!=13 && current!=-1 && current!=10) {
 							wordCount++;
 							previous=current;
 						}
-						while((current=reader.read())!=-1) //previous=g current=g
-						{
-							if((previous==10 || previous==32) && (current!=13 && current!=32))
-							{
-								wordCount++;
+						while((current=reader.read())!=-1) {
+							if((previous==10 || previous==32) && (current!=13 && current!=32)) {
 								
+								wordCount++;
 							}
+							
 							previous=current;
-							
-							
-							
 						}
 				
-					
 					System.out.println("Word Count: "+ wordCount);
-				
-				
 				}
-				finally
-				{
-					if(reader!=null)
-					{
+				finally {
+					if(reader!=null) {
 						reader.close();
 					}
 				}
-			
 		}
 	}
 	
 	public static boolean parseArgs(String [] args)
 	{
-		boolean bool=true;
+		boolean correctArgs=true;
 		int i = 0;
 		
-		if(args.length==0)
-		{
-		
-			bool=false;
+		if(args.length==0) {
+			
+			correctArgs=false;
 		}
-		
-		while(i<args.length)
-		{
-			if(args[i].equals("-w"))
-			{
+		while(i<args.length) {
+			if(args[i].equals("-w")) {
+				
 				i++;
 			}
-			else
-			{
+			else {
 				File inputFile;
-				try
-				{
 				
-				inputFile = new File(args[i]);
+				try {
+					inputFile = new File(args[i]);
+					
 					if(!inputFile.exists())
 					{
-						bool=false;
+						correctArgs=false;
 					}
-				i++;
+					
+					i++;
 				}
 				catch(Exception e)
 				{
-					bool=false;
+					correctArgs=false;
 					break;
 				}
 			}
 		}
 		
-		if(args.length==1)
-		{
-			bool=false;
+		if(args.length==1) {
+			correctArgs=false;
 		}
 		
-		return bool;
+		return correctArgs;
 	}
 }
